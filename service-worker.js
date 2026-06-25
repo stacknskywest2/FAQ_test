8db8afcdc28cb9be9fac3109f7bcef80f8e83f9b
stacknskywest2 <stacknsky_west2@keco.or.kr>
2026-06-25 09:40:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gulttuk-helper-v16-1-4-20260624-manual-cover-cache-bust';
+const CACHE_NAME = 'gulttuk-helper-v16-1-5-20260624-intro-logo-fix';
 const PRECACHE_URLS = [
   "./",
   "./index.html",
@@ -14,7 +14,7 @@ const PRECACHE_URLS = [
   "./icons/ttumi-check.png",
   "./icons/ttumi-guide.png",
   "./icons/keco-logo.png",
-  "./icons/keco-logo-intro.png",
+  "./icons/keco-logo-intro-v16_1_5.png",
   "./icons/ttumi-basic-transparent.png",
   "./images/FAQ-D-001_system_overview_access_p280.jpg",
   "./images/FAQ-D-002_signup_login_flow_p281.jpg",
